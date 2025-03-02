c748bf8d147a7e28decfd3586f005f42d6719fdf
umrn5505 <umrn5505@gmail.com>
2025-03-02 19:46:06 +0500

    Changes

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask,render_template, request
+from flask import Flask, render_template, request
 from auth import auth_bp  # Import only the Blueprint
-from students import student_bp
-from assessment import assessment_bp# Import the Blueprint
+from students import student_bp  # Import the Blueprint
+from teacher import teacher_bp  # Import the Blueprint
 from database import Database
 from auth import Auth
+
 auth = Auth()
 
 app = Flask(__name__)
@@ -12,10 +13,7 @@ app.secret_key = 'TRY/One12'
 # Register Blueprints
 app.register_blueprint(auth_bp, url_prefix='/auth')
 app.register_blueprint(student_bp, url_prefix='/student')
-app.register_blueprint(assessment_bp, url_prefix='/assessment')
-
-
-
+app.register_blueprint(teacher_bp, url_prefix='/teacher')  # Register the teacher Blueprint
 
 @app.route('/', methods=['GET', 'POST'])
 @auth.login_required('admin')
@@ -23,7 +21,6 @@ def index():
     if request.method == 'POST':
         return "Form submitted!", 200
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
